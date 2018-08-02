ceab4e54686a850fa693b7d8059a87ab8cf91764
ShanwNew <ncxinhanzhong@gmail.com>
2018-08-02 17:36:39 +0800

    check and remove file before open

--- config.py
+++ config.py
@@ -6,8 +6,19 @@ FRAME = [320, 160]
 base_path = os.getcwd()
 output_file_dir = 'video/'
 output_file_name = 'capturedVideo.avi'
+DIR_PATH = os.path.join(base_path, output_file_dir)
 
-PATH = os.path.join(base_path, output_file_dir, output_file_name)
+if os.path.exists(DIR_PATH):
+    temp_file_path = os.path.join(DIR_PATH, output_file_name)
+    if os.path.exists(temp_file_path):
+        os.remove(temp_file_path)
+    PATH = temp_file_path
+else:
+    os.mkdir(DIR_PATH)
+    temp_file_path = os.path.join(DIR_PATH, output_file_name)
+    if os.path.exists(temp_file_path):
+        os.remove(temp_file_path)
+    PATH = temp_file_path
 
 
 
