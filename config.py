43b74ae43b16791febba231b8881511833dc8c26
admin <admin@admindeMacBook-Pro.local>
2018-08-02 16:31:45 +0800

    add saving video

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+FPS = 30
+FRAME = [320, 160]
+
+base_path = os.getcwd()
+output_file_dir = 'video/'
+output_file_name = 'capturedVideo.avi'
+
+PATH = os.path.join(base_path, output_file_dir, output_file_name)
+
+
+
+
